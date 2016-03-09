8c525cc0902e2d30ebbea159c30a2197b94bba13
Diogo Moura <Diogo Moura>
2016-03-09 20:21:30 +0000

    added new chart and href in some pages

--- piu/js/charts.js
+++ piu/js/charts.js
@@ -132,24 +132,42 @@
         
         var data = [
     {
-        value: 300,
+        value: 50,
         color:"#F7464A",
         highlight: "#FF5A5E",
-        label: "Red"
+        label: "Amigos"
     },
     {
-        value: 50,
+        value: 1,
         color: "#46BFBD",
         highlight: "#5AD3D1",
-        label: "Green"
+        label: "Administrador"
     },
     {
-        value: 100,
+        value: 2,
         color: "#FDB45C",
         highlight: "#FFC870",
-        label: "Yellow"
+        label: "Contabilistas"
     }
 ]
         var ctx = document.getElementById("myChart").getContext("2d");
         // For a pie chart
 var myPieChart = new Chart(ctx).Pie(data);
+
+var dataBar = {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [
+        {
+            label: "My First dataset",
+            fillColor: "rgba(220,220,220,0.5)",
+            strokeColor: "rgba(220,220,220,0.8)",
+            highlightFill: "rgba(220,220,220,0.75)",
+            highlightStroke: "rgba(220,220,220,1)",
+            data: [65, 59, 80, 81, 56, 55, 40]
+        }
+    ]
+};
+
+var ctxbar = document.getElementById("earnings").getContext("2d");
+
+var barChart = new Chart(ctxbar).Bar(dataBar);
