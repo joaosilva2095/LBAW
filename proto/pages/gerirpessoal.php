db14885e8de83a411e3785a93d89517c6b8e268c
João Silva <joaosilva@revtut.net>
2016-04-24 15:57:24 +0100

    Improved validation of the user.

--- proto/pages/gerirpessoal.php
+++ proto/pages/gerirpessoal.php
@@ -2,21 +2,14 @@
 include_once('../config/init.php');
 include_once($BASE_DIR.'database/users.php');
 
-// Check if the user is logged in
-if (!isset($_SESSION['username'])) {
+// Validate user
+if (!isset($_SESSION['username']) || $_SESSION['role'] !== 'Administrador') {
     $_SESSION['error_messages'][] = 'No permission to access this page!';
     http_response_code(404);
     return;
 }
 
-// Check permission to access the page
 $user = get_user_by_name($_SESSION['username']);
-if ($user['role'] !== 'Administrador') {
-    $_SESSION['error_messages'][] = 'No permission to access this page!';
-    http_response_code(404);
-    return;
-}
-
 $users = get_all_users();
 $notifications = array();
 
