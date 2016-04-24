3f5d0499725f29c84fd7e5fe7df4d1899dfba24d
João Silva <joaosilva@revtut.net>
2016-04-24 11:58:39 +0100

    Added email validation.

--- proto/api/register_user.php
+++ proto/api/register_user.php
@@ -3,8 +3,7 @@ include_once('../../config/init.php');
 include_once($BASE_DIR.'database/users.php');
 
 // Check if the user is logged in
-if(!isset($_SESSION['username']))
-    return;
+if (!isset($_SESSION['username'])) return;
 
 // TODO Check permissions
 
@@ -18,7 +17,7 @@ $role = $_POST['role'];
 
 // Check if all parameters exist
 $params = array('id', 'email', 'password', 'name', 'gender', 'birth');
-if ($role === 'Amigo') array_push($params, 'nif', 'cellphone', 'donative_type', 'periodicity', 'donative_amount');
+if ($role === 'Amigo') array_push($params, 'nif', 'cellphone', 'donative_type', 'periodicity');
 
 foreach($params as $param) {
     if (!isset($_POST[$param])) {
@@ -28,6 +27,13 @@ foreach($params as $param) {
     } else {
         $params[$param] = $_POST[$param];
     }
+}
+
+// Validate input parameters
+if (!filter_var($params['email'], FILTER_VALIDATE_EMAIL)) {
+    $_SESSION['error_messages'][] = 'Invalid input emails!';
+    header('', true, 404);
+    return;
 }
 
 // Insert in the database
